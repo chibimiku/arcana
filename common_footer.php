0d81460669b280540fad9c341a1b559ad83057a1
chibimiku <dreamcosine@gmail.com>
2018-07-23 00:50:46 +0800

    small fix
    only small fix~~

--- common_footer.php
+++ common_footer.php
@@ -15,8 +15,6 @@ $page_cost = time() - TIMESTAMP;
 </div>
 
 <div id="said_calculation">
-	<script language="javascript" type="text/javascript" src="http://js.users.51.la/5962403.js"></script>
-	<noscript><a href="http://www.51.la/?5962403" target="_blank"><img alt="&#x6211;&#x8981;&#x5566;&#x514D;&#x8D39;&#x7EDF;&#x8BA1;" src="http://img.users.51.la/5962403.asp" style="border:none" /></a></noscript>
 	<!-- 度娘统计 -->
 	<script>
 	(function(){
